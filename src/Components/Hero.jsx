bfed0412d3f62a5b7caff895bc65755fd5c72be1
Alepepi <aleollivierochoa@gmail.com>
2024-05-27 11:54:52 -0600

    Change of typography name

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                     variants={container(0)}
                     initial="hidden"
                     animate="visible"
-                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                    className="pb-16 text-6xl font-extralight tracking-tight lg:mt-16 lg:text-8xl">
                         Alejandro Ollivier
                     </motion.h1>
                     <motion.span
